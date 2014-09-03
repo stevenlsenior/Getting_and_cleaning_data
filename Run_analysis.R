279f5c0a5830fb6e52f57c782e00d6d72b38c868
Steven Senior <steven.l.senior@gmail.com>
2014-09-03 07:07:48 +0100

    skeleton outline with key task headings

---
+++ Run_analysis.R
@@ -0,0 +1,15 @@
+## This R script is the programming assignment for the Coursera course 'getting and cleaning data'
+
+## The script processes a data set containing accellerometer data collected from a smartphone.
+## The data was provided by the course and can be found at: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## 1. Download data
+
+## 2. Merge training and test data sets to create one data set
+
+## 3. Extracts only the measurements on the mean and standard deviation for each data set
+
+## 4. Uses descriptive activity names to name activities in the data set
+
+## 5. Create a second, independent tidy data set with the average of each variable for each activity and subject
+
